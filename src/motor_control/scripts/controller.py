b3e5e0947a23222962a53e72c65c7d2999229179
Gabe Rodriguez <gabe.rodriguez78@gmail.com>
2019-03-31 10:58:04 -0700

    working controller.py

--- src/motor_control/scripts/controller.py
+++ src/motor_control/scripts/controller.py
@@ -5,14 +5,18 @@ import RPi.GPIO as GPIO
 
 mcPins = [6, 13, 19, 26] # pins used for motor control
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(6, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(mcPins, GPIO.OUT, initial=GPIO.LOW)
 
 
 def controller():
-
+	## Convert angular velocity signal into individual PWM signals
 	rospy.init_node('controller', anonymous=True)
 	rate = rospy.Rate(10) # 10hz
+
+	# Set pins 6, 19 to HIGH
+	GPIO.output(6, 1)
+	GPIO.output(19, 1)
 	while not rospy.is_shutdown():
 		hello_str = "Hello world!"
 		rospy.loginfo(hello_str)
