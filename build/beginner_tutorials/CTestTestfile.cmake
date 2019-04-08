8187668cc2788c0dc99fe9e6309a15561f5acddf
Gabe Rodriguez <gabe.rodriguez78@gmail.com>
2019-04-07 21:04:43 -0700

    ignoring build, devel, install dirs

--- build/beginner_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gabe/sandbox/src/beginner_tutorials
-# Build directory: /home/gabe/sandbox/build/beginner_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
