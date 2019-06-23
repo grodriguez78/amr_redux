673879617618ca77f7c9a57ffcb54a1f041246b4
Gabe Rodriguez <gabe.rodriguez78@gmail.com>
2019-06-23 12:35:42 -0700

    directional pwm on left wheel

--- src/motor_control/scripts/controller.py
+++ src/motor_control/scripts/controller.py
@@ -9,25 +9,42 @@ mcPins = [6, 13, 19, 26] # pins used for motor control
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(mcPins, GPIO.OUT, initial=GPIO.LOW)
 
+left_fwd = GPIO.PWM(6, 100)
+left_bkwds = GPIO.PWM(13, 100)
+
 
 def callback(data):
 	rospy.loginfo(rospy.get_caller_id() + " Setting right wheel: %d", data.rightVelocity)
 	rospy.loginfo(rospy.get_caller_id() + " Setting left wheel: %d", data.leftVelocity)
 
+	if data.leftVelocity > 0:
+		left_bkwds.stop()
+		left_fwd.start(50)
+
+	elif data.leftVelocity < 0:
+		left_fwd.stop()
+		left_bkwds.start(50)
+
+	else: 
+		left_fwd.stop()
+		left_bkwds.stop()
+
+
 def controller():
 	## Convert angular velocity signal into individual PWM signals
 	rospy.init_node('controller', anonymous=True)
 
-	rospy.Subscriber("robot_dynamics", WheelVelocity, callback)
+	s = rospy.Subscriber("robot_dynamics", WheelVelocity, callback)
 
 	rate = rospy.Rate(10) # 10hz
+
 
 	# Set pins 6, 19 to HIGH
 	GPIO.output(6, 1)
 	GPIO.output(19, 1)
 	while not rospy.is_shutdown():
-		hello_str = "Hello world!"
-		rospy.loginfo(hello_str)
+		# hello_str = "Hello world!"
+		# rospy.loginfo(hello_str)
 		rate.sleep()
 
 if __name__ == '__main__':
