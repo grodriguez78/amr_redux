eadb5f8dc44589bcef342a5f5a05f2aa51c14948
Gabe Rodriguez <gabe.rodriguez78@gmail.com>
2019-07-16 21:58:01 -0700

    state machine working

--- src/remote_command/src/command_node.py
+++ src/remote_command/src/command_node.py
@@ -15,6 +15,22 @@ AXIS_NAMES = {
 	5: 'trig_right'
 }
 
+BUTTON_NAMES = {
+	0: 'cross', 
+	1: 'circle',
+	2: 'triangle',
+	3: 'square',
+	4: 'left_bumper',
+	5: 'right_bumper',
+	6: 'trig_left',
+	7: 'trig_right',
+	8: 'share',
+	9: 'options',
+	10: 'home',
+	11: 'left_stick',
+	12: 'right_stick'
+}
+
 def pub_commands():
 	## Read inputs from Dualshock Controller & publish to remote_commands message
 
@@ -33,12 +49,17 @@ def pub_commands():
 	rate = rospy.Rate(10)
 	while not rospy.is_shutdown():
 
-		## Update all joystick values on every joystick event
+		## Update joystick and button values
 		for event in pygame.event.get():
 			if event.type == pygame.JOYAXISMOTION:
 				for axis in AXIS_NAMES.keys():
 					setattr(inputs, AXIS_NAMES[axis], controller.get_axis(axis))
 				pub.publish(inputs)
+			if event.type == pygame.JOYBUTTONDOWN:
+				setattr(inputs, BUTTON_NAMES[event.button], True)
+				pub.publish(inputs)
+			if event.type == pygame.JOYBUTTONUP:
+				setattr(inputs, BUTTON_NAMES[event.button], False)
 		rate.sleep()
 
 if __name__ == '__main__':
--- src/system_state/src/worker_state_machine.py
+++ src/system_state/src/worker_state_machine.py
@@ -4,6 +4,7 @@ import time
 import rospy
 
 from system_state.msg import WorkerState
+from remote_command.msg import DualshockInputs
 
 STATE_NAME_MAP= {
 	0: 'CHARGING/INACTIVE',
@@ -11,40 +12,76 @@ STATE_NAME_MAP= {
 	2: 'AUTONOMOUS CONTROL'
 }
 
+class WorkerStateMachine():
 
-def worker_state_machine():
-	## Determine the current state of this worker
-
-	## TODO: Name node based on worker ID
-	rospy.init_node('lilboi_state_machine', anonymous=True)
-	pub = rospy.Publisher('lilboi_state', WorkerState, queue_size=10)
-
-	## Default to state 0 (CHARGING/INACTIVE)
-	state = 0
-	pub.publish(state)
-	state_change_msg = 'Lilboi is now in state %i: (%s)' %(state, STATE_NAME_MAP[state]) 
-	rospy.loginfo(state_change_msg)
-
-	rate = rospy.Rate(10)
-	while not rospy.is_shutdown():
-
-		# if state == 0:
-		# 	if triangle is pressed: state = 1 
-		# 	if charged and task is available: state = 2
-
-		# elif state == 1:
-		# 	if battery is depleted: state = 0
-		#	if triangle is pressed: state = 2
-
-		# elif state == 2:
-		# 	if battery is depleted: state = 0
-		# 	if trangle is pressed: state = 1
+	def __init__(self):
+		self.state = 0
+		self.pub = None
+		self.sub = None
+		self.triangle = None
 
 
-		rate.sleep()
+	def triangle_pressed(self, triangle_state):
+		return not self.triangle and triangle_state
+
+
+	def change_state(self, new_state):
+		state_change_msg = 'Lilboi transitioned from state %i (%s) to state %i (%s)'%(self.state, STATE_NAME_MAP[self.state], new_state, STATE_NAME_MAP[new_state])
+		rospy.loginfo(state_change_msg)
+		self.state = new_state
+
+
+	def user_input_transitions(self, data):
+
+		## Charging
+		if self.state == 0:
+			if self.triangle_pressed(data.triangle): self.change_state(1)
+
+		elif self.state == 1:
+			if self.triangle_pressed(data.triangle): self.change_state(2)
+
+		elif self.state == 2:
+			if self.triangle_pressed(data.triangle): self.change_state(1)
+
+		## Update local values for next iteration
+		self.triangle = data.triangle
+
+
+	def run_state_machine(self):
+		## Determine the current state of this worker
+
+		## TODO: Name node based on worker ID
+		rospy.init_node('lilboi_state_machine', anonymous=True)
+		self.pub = rospy.Publisher('lilboi_state', WorkerState, queue_size=10)
+		self.sub = rospy.Subscriber('remote_commands', DualshockInputs, self.user_input_transitions)
+
+		## Default to state 0 (CHARGING/INACTIVE)
+		state = 0
+		state_change_msg = 'Lilboi is now in state %i: (%s)' %(state, STATE_NAME_MAP[state]) 
+		rospy.loginfo(state_change_msg)
+
+		rate = rospy.Rate(10)
+		while not rospy.is_shutdown():
+			self.pub.publish(self.state)
+
+			# if state == 0:
+			# 	if triangle is pressed: state = 1 
+			# 	if charged and task is available: state = 2
+
+			# elif state == 1:
+			# 	if battery is depleted: state = 0
+			#	if triangle is pressed: state = 2
+
+			# elif state == 2:
+			# 	if battery is depleted: state = 0
+			# 	if trangle is pressed: state = 1
+
+
+			rate.sleep()
 
 if __name__ == '__main__':
 	try:
-		worker_state_machine()
+		n = WorkerStateMachine()
+		n.run_state_machine()
 	except rospy.ROSInterruptException:
 		pass
