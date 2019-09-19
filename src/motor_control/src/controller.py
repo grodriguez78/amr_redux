ff784b476a4c5a92967aa8e88fd1ff6bb4164d02
Gabe Rodriguez <gabe.rodriguez78@gmail.com>
2019-09-18 22:53:48 -0700

    created Controller class

--- src/motor_control/src/controller.py
+++ src/motor_control/src/controller.py
@@ -9,158 +9,177 @@ import RPi.GPIO as GPIO
 from motor_control.msg import WheelVelocity
 from utils import scale_velocity
 
-mcPins = [16, 20, 19, 26] 	# GPIO pins used for motor control
-encPins = [13, 6, 24, 23] 	# GPIO pins used to read encoders
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(mcPins, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(encPins, GPIO.IN)
+class Controller():
 
-## Control pins
-left_fwd = GPIO.PWM(26, 100)
-left_bkwds = GPIO.PWM(19, 100)
-right_fwd = GPIO.PWM(16, 100)
-right_bkwds = GPIO.PWM(20, 100)
+	def __init__():
+		""" Initialize class
 
-## Encoder pins
-left_clk = 6
-left_dt = 13
-right_clk = 23
-right_dt = 24
+		"""
 
-## Storage for encoder trigger times
-LAST_TRIG_LEFT = 0.0
-LAST_TRIG_RIGHT = 0.0
+		# Setup GPIO
+		self.encPins = [13, 6, 24, 23] 	# GPIO pins used to read encoders
+		self.mcPins = [16, 20, 19, 26] 	# GPIO pins used for motor control
 
-## Angular distance of a single encoder step, in radians
-KY_040_STEP = 2 * np.pi / 30
-
-## Calculate bouncetime (ms) from max angular velocity
-BOUNCETIME = int(1000 * 0.5 * KY_040_STEP / 20)
-
-def encoder_direction(clk, dt):
-	""" Determine encoder direction from the current state of clk and dt pins
+		GPIO.setwarnings(False)
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.mcPins, GPIO.OUT, initial=GPIO.LOW)
+		GPIO.setup(self.encPins, GPIO.IN)
+	
+		## Hardware specs
 		
-		clk 	pin number for an encoder's clk output
-		dt 		pin number for an encoder's dt output
+		# Encoder Pins
+		self.left_clk = 6
+		self.left_dt = 13
+		self.right_clk = 23
+		self.right_dt = 24
 
-		return   1 - clockwise direction
-			    -1 - counter-clockwise direction
-	"""
+		# Control pins
+		self.left_fwd = GPIO.PWM(26, 100)
+		self.left_bkwds = GPIO.PWM(19, 100)
+		self.right_fwd = GPIO.PWM(16, 100)
+		self.right_bkwds = GPIO.PWM(20, 100)
 
-	# Wheel is rotating clockwise
-	if GPIO.input(left_clk) != GPIO.input(left_dt):
-		direction = 1
+		# Angular distance of a single encoder step, in radians
+		self.enc_step_size = 2 * np.pi / 30
 
-	# Wheel is rotating counter-clockwise
-	else:
-		direction = -1
+		# Calculate bouncetime (ms) from max angular velocity
+		self.bouncetime = int(1000 * 0.5 * self.enc_step_size / 20)
 
-	return direction
+		# Storage for past encoder trigger times
+		self.last_trig_left = 0.0
+		self.last_trig_right = 0.0
 
-def left_up(channel):
-	""" Note the time the left encoder's clk went high and determine which
-		direction the left wheel is turning
-	"""
-
-	# Record trigger time
-	t_trig = time.time()
-
-	# Determine rotation direction
-	direction = encoder_direction(left_clk, left_dt)
-
-	# Calculate angular velocity
-	d_t = t_trig - LAST_TRIG_LEFT
-	w = direction * KY_040_STEP / d_t
-
-	# Reset recorded trigger time
-	LAST_TRIG_LEFT = t_trig
-
-	print "Left Wheel Angular Velocity: %0.2f"%w
+		# Wheel velocities
+		self.w_left_meas = 0.0
+		self.w_right_meas = 0.0
 
 
-def right_up(channel):
-	""" Note the time the right encoder's clk went high and determine which
-		direction the wheel is turning
-	"""
+	def encoder_direction(wheel):
+		""" Determine encoder direction from the current state of clk and dt pins
+			
+			clk 	pin number for an encoder's clk output
+			dt 		pin number for an encoder's dt output
 
-	# Record trigger time
-	t_trig = time.time()
+			return   1 - clockwise direction
+				    -1 - counter-clockwise direction
+		"""
 
-	# Determine rotation direction
-	direction = encoder_direction(right_clk, right_dt)
+		assert wheel in ['left', 'right'], "Wheel can only be left or right!"
 
-	# Calculate angular velocity
-	d_t = t_trig - LAST_TRIG_RIGHT
-	w = direction * KY_040_STEP / d_t
+		# Wheel is rotating clockwise
+		if GPIO.input(getattr(self, wheel + '_clk')) != GPIO.input(getattr(self, wheel + '_dt')):
+			direction = 1
 
-	# Reset recorded trigger time
-	LAST_TRIG_RIGHT = t_trig
+		# Wheel is rotating counter-clockwise
+		else:
+			direction = -1
 
-	print "Right Wheel Angular Velocity: %0.2f"%w
+		return direction
 
+	def left_up(channel):
+		""" Note the time the left encoder's clk went high and determine which
+			direction the left wheel is turning
+		"""
 
-def set_wheel_velocities(w_left, w_right):
-	""" Drive left and right wheels at angular velocities w_l, w_r
+		# Record trigger time
+		t_trig = time.time()
 
-	"""
+		# Determine rotation direction
+		direction = self.encoder_direction('left')
 
-	## Left wheel
-	if w_left > 0:
-		left_bkwds.stop()
-		left_fwd.start(scale_velocity(w_left))
+		# Calculate angular velocity
+		d_t = t_trig - self.last_trig_left
+		self.w_left = direction * KY_040_STEP / d_t
 
-	elif w_left < 0:
-		left_fwd.stop()
-		left_bkwds.start(scale_velocity(w_left))
+		# Reset recorded trigger time
+		self.last_trig_left = t_trig
 
-	else: 
-		left_fwd.stop()
-		left_bkwds.stop()
-
-	## Right wheel
-	if w_right > 0:
-		right_bkwds.stop()
-		right_fwd.start(scale_velocity(w_right))
-
-	elif w_right < 0:
-		right_fwd.stop()
-		right_bkwds.start(scale_velocity(w_right))
-
-	else:
-		right_fwd.stop()
-		right_bkwds.stop()
+		print "Left Wheel Angular Velocity: %0.2f"%self.w_left
 
 
-def callback(data):
-	rospy.loginfo(rospy.get_caller_id() + " w_left: %d", data.w_left)
-	rospy.loginfo(rospy.get_caller_id() + " w_right: %d", data.w_right)
+	def right_up(channel):
+		""" Note the time the right encoder's clk went high and determine which
+			direction the wheel is turning
+		"""
 
-	set_wheel_velocities(data.w_left, data.w_right)
+		# Record trigger time
+		t_trig = time.time()
+
+		# Determine rotation direction
+		direction = self.encoder_direction('right')
+
+		# Calculate measured angular velocity
+		d_t = t_trig - self.last_trig_right
+		self.w_right_meas = direction * KY_040_STEP / d_t
+
+		# Reset recorded trigger time
+		self.last_trig_right = t_trig
+
+		print "Right Wheel Angular Velocity: %0.2f"%self.w_right_meas
 
 
-def controller():
-	## Convert angular velocity signal into individual PWM signals
-	rospy.init_node('controller', anonymous=True)
+	def set_wheel_velocities(w_left, w_right):
+		""" Drive left and right wheels at angular velocities w_l, w_r
 
-	s = rospy.Subscriber("robot_dynamics", WheelVelocity, callback)
+		"""
 
-	rate = rospy.Rate(10) # 10hz
+		## Left wheel
+		if w_left > 0:
+			left_bkwds.stop()
+			left_fwd.start(scale_velocity(w_left))
 
-	LAST_TRIG_LEFT = time.time()
-	LAST_TRIG_RIGHT = time.time()
+		elif w_left < 0:
+			left_fwd.stop()
+			left_bkwds.start(scale_velocity(w_left))
 
-	# Setup encoder interrupt callbacks
-	GPIO.add_event_detect(left_clk, GPIO.RISING, callback= left_up, bouncetime= BOUNCETIME)
-	GPIO.add_event_detect(right_clk, GPIO.RISING, callback= right_up, bouncetime= BOUNCETIME)
+		else: 
+			left_fwd.stop()
+			left_bkwds.stop()
 
-	while not rospy.is_shutdown():
-		rate.sleep()
+		## Right wheel
+		if w_right > 0:
+			right_bkwds.stop()
+			right_fwd.start(scale_velocity(w_right))
+
+		elif w_right < 0:
+			right_fwd.stop()
+			right_bkwds.start(scale_velocity(w_right))
+
+		else:
+			right_fwd.stop()
+			right_bkwds.stop()
+
+
+	def callback(data):
+		rospy.loginfo(rospy.get_caller_id() + " w_left: %d", data.w_left)
+		rospy.loginfo(rospy.get_caller_id() + " w_right: %d", data.w_right)
+
+		self.set_wheel_velocities(data.w_left, data.w_right)
+
+
+	def run():
+		## Convert angular velocity signal into individual PWM signals
+		rospy.init_node('controller', anonymous=True)
+
+		s = rospy.Subscriber("robot_dynamics", WheelVelocity, callback)
+
+		rate = rospy.Rate(10) # 10hz
+
+		self.last_trig_left = time.time()
+		self.last_trig_right = time.time()
+
+		# Setup encoder interrupt callbacks
+		GPIO.add_event_detect(self.left_clk, GPIO.RISING, callback= self.left_up, bouncetime= BOUNCETIME)
+		GPIO.add_event_detect(self.right_clk, GPIO.RISING, callback= self.right_up, bouncetime= BOUNCETIME)
+
+		while not rospy.is_shutdown():
+			rate.sleep()
 
 if __name__ == '__main__':
 	try:
-		controller()
+		controller = Controller()
+		controller.run()
 	except rospy.ROSInterruptException:
 		GPIO.cleanup()
 		pass
