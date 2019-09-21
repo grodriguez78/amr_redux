2336849dde747f946ec75ec44ce3f70f32dcabe9
Gabe Rodriguez <gabe.rodriguez78@gmail.com>
2019-09-20 22:56:32 -0700

    updated encoder measurement method

--- src/motor_control/src/controller.py
+++ src/motor_control/src/controller.py
@@ -29,10 +29,10 @@ class Controller():
 		## Hardware specs
 		
 		# Encoder Pins
-		self.left_clk = 6
-		self.left_dt = 13
-		self.right_clk = 23
-		self.right_dt = 24
+		self.left_wheel_clk = 6
+		self.left_wheel_dt = 13
+		self.right_wheel_clk = 23
+		self.right_wheel_dt = 24
 
 		# Control pins
 		self.left_fwd = GPIO.PWM(26, 100)
@@ -46,77 +46,141 @@ class Controller():
 		# Calculate bouncetime (ms) from max angular velocity
 		self.bouncetime = int(1000 * 0.5 * self.enc_step_size / 20)
 
-		# Storage for past encoder trigger times
-		self.last_trig_left = 0.0
-		self.last_trig_right = 0.0
+		# Encoder identifiers
+		self.encoder_ids = ['left_wheel', 'right_wheel']
+
+		# Storage for encoder step counts
+		self.left_wheel_step_count = 0
+		self.right_wheel_step_count = 0
+
+		# Storage for current encoder direction
+		#  1 - Clockwise
+		#  0 - Unset / stopped
+		# -1 - Counter-Clockwise
+		self.left_wheel_dir = 0
+		self.right_wheel_dir = 0
+
+		# Accepted string values for encoder directions
+		self.enc_direction_map = {
+			-1: ['ccw', 'counter-clockwise']
+			0:  ['stopped'],
+			1:  ['cw', 'clockwise']
+		}
+
+		# Storage for encoder resets 
+		self.left_wheel_reset_time = None
+		self.right_wheel_reset_time = None
 
 		# Wheel velocities
-		self.w_left_meas = 0.0
-		self.w_right_meas = 0.0
+		self.left_wheel_meas_w = 0.0
+		self.right_wheel_meas_w = 0.0
 
 
-	def encoder_direction(self, wheel):
-		""" Determine encoder direction from the current state of clk and dt pins
+
+	def meas_encoder_direction(self, encoder_id):
+		""" Measure encoder direction from the current state of dt pin.
+
+			Due to the rising edge triggers set on encoder clk outputs, it can
+			be assumed that clk is high when this function is called.
 			
 			clk 	pin number for an encoder's clk output
 			dt 		pin number for an encoder's dt output
 
-			return   1 - clockwise direction
-				    -1 - counter-clockwise direction
+			return   1 - clockwise 
+				    -1 - counter-clockwise 
 		"""
 
-		assert wheel in ['left', 'right'], "Wheel can only be left or right!"
+		# Ensure specified encoder exists
+		assert encoder_id in self.encoder_ids, "Specified encoder %s does not exist!"%encoder_id
 
-		# Wheel is rotating clockwise
-		if GPIO.input(getattr(self, wheel + '_clk')) != GPIO.input(getattr(self, wheel + '_dt')):
-			direction = 1
+		# d_t is HIGH (Encoder is rotating counter-clockwise)
+		if GPIO.input(getattr(self, encoder_id + '_dt')):
+			return -1
 
-		# Wheel is rotating counter-clockwise
+		# d_t is LOW (Encoder is rotating clockwise)
 		else:
-			direction = -1
+			return 1
 
-		return direction
+
+	def record_encoder_tick(self, encoder_id):
+		""" Update encoder step counter or reset if direction has changed
+
+		"""
+
+		# Ensure specified encoder exists
+		assert encoder_id in self.encoder_ids, "Specified encoder %s does not exist!"%encoder_id
+
+		meas_dir = self.meas_encoder_direction(encoder_id)
+
+		# Check if encoder has changed direction
+		if getattr(self, encoder_id + '_dir') != meas_dir:
+			self.reset_encoder(encoder_id)
+			self.set_encoder_direction(encoder_id, meas_dir)
+
+		else:
+			# Increment encoder step count
+			getattr(self, encoder_id + '_step_count')
+
+	def reset_encoder(self, encoder_id):
+		""" Set encoder step count to 0 and record reset time
+
+		"""
+
+		getattr(self, encoder_id + '_reset_time') = time.time()
+		getattr(self, encoder_id + '_step_count') = 0
+
+	def set_encoder_direction(self, encoder_id, direction):
+		""" Update the stored encoder direction
+
+			:params:
+			encoder_id - Unique encoder identifier (string)
+			direction  - Encoder's rotation direction. May be integer or string 
+		"""
+
+		# Ensure specified encoder exists
+		assert encoder_id in self.encoder_ids, "Specified encoder %s does not exist!"%encoder_id
+
+		if isinstance(direction, int):
+			assert direction in self.enc_direction_map.keys(), "Specified encoder direction %i is not valid!"%direction
+		elif isintance(direction, str):
+			allowed_strings = [item for strings in self.enc_direction_map.values() for item in strings]
+			assert direction.lower() in allowed_strings, "Specified encoder direction %s is not valid!"%direction
+			for key, values in self.enc_direction_map.items():
+				if direction.lower() in values:
+					direction = key
+
+		getattr(self, encoder_id + '_dir') = direction
+
+	def get_encoder_direction(self, encoder_id):
+		""" Return the stored encoder direction
+
+		"""
+		return getattr(self, encoder_id + '_dir')
+
+	def get_encoder_reset_time(self, encoder_id):
+		""" Return the recorded encoder reset time
+
+		"""
+		return getattr(self, encoder_id + '_reset_time')
+
+
+	def get_encoder_step_count(self, encoder_id):
+		""" Return the encoder's current step count
+
+		"""
+		return getattr(self, encoder_id, '_step_count')
+
 
 	def left_up(self, channel):
-		""" Note the time the left encoder's clk went high and determine which
-			direction the left wheel is turning
+		""" Wrapper to apply record_encoder_tick during GPIO Interrupt callback
 		"""
-
-		# Record trigger time
-		t_trig = time.time()
-
-		# Determine rotation direction
-		direction = self.encoder_direction('left')
-
-		# Calculate angular velocity
-		d_t = t_trig - self.last_trig_left
-		self.w_left = direction * self.enc_step_size / d_t
-
-		# Reset recorded trigger time
-		self.last_trig_left = t_trig
-
-		print "Left Wheel Angular Velocity: %0.2f"%self.w_left
+		self.record_encoder_tick('left_wheel')
 
 
 	def right_up(self, channel):
-		""" Note the time the right encoder's clk went high and determine which
-			direction the wheel is turning
+		""" Wrapper to apply record_encoder_tick during GPIO Interrupt callback
 		"""
-
-		# Record trigger time
-		t_trig = time.time()
-
-		# Determine rotation direction
-		direction = self.encoder_direction('right')
-
-		# Calculate measured angular velocity
-		d_t = t_trig - self.last_trig_right
-		self.w_right_meas = direction * self.enc_step_size / d_t
-
-		# Reset recorded trigger time
-		self.last_trig_right = t_trig
-
-		print "Right Wheel Angular Velocity: %0.2f"%self.w_right_meas
+		self.record_encoder_tick('right_wheel')
 
 
 	def set_wheel_velocities(self, w_left, w_right):
@@ -170,10 +234,24 @@ class Controller():
 		self.last_trig_right = time.time()
 
 		# Setup encoder interrupt callbacks
+		## TODO: Generalize this with a lambda call so a unique function is not needed
+		#  		 for each encoder 
 		GPIO.add_event_detect(self.left_clk, GPIO.RISING, callback= self.left_up, bouncetime= self.bouncetime)
 		GPIO.add_event_detect(self.right_clk, GPIO.RISING, callback= self.right_up, bouncetime= self.bouncetime)
 
 		while not rospy.is_shutdown():
+
+			# Calculate measured angular velocity
+			for encoder_id in self.encoder_ids:
+
+				# Calculate time since last reset
+				d_t = time.time() - get_encoder_reset_time(encoder_id)
+				w_meas = self.get_encoder_direction(encoder_id) * self.get_encoder_step_count(encoder_id) * self.enc_step_size / d_t
+				print "Encoder %s: Measured angular velocity %0.2f"%(encoder_id, w_meas) 
+
+				# Restart encoder count
+				self.reset_encoder(encoder_id)
+
 			rate.sleep()
 
 if __name__ == '__main__':
